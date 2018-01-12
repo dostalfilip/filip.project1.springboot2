f9bacad60a1efef8d1f5ea677586e7f0a7e3ea8f
Filip Dostal <dostal.filip@hotmail.com>
2018-01-13 00:20:56 +0100

    init

---
+++ src/main/java/com/example/filip/project1/pojo/Author.java
@@ -0,0 +1,93 @@
+package com.example.filip.project1.pojo;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Author {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String firstName, lastName;
+	
+	@ManyToMany(mappedBy = "authors")
+	private Set<Book> books = new HashSet<>();
+		
+	public Author() {
+		
+	}
+
+	public Author(String firstName, String lastName, Set<Book> books) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.books = books;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Set<Book> getBooks() {
+		return books;
+	}
+
+	public void setBooks(Set<Book> books) {
+		this.books = books;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((books == null) ? 0 : books.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Author other = (Author) obj;
+		if (books == null) {
+			if (other.books != null)
+				return false;
+		} else if (!books.equals(other.books))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
